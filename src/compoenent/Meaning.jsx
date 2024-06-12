d363a9fa8a12ffe2fffacd8972d369d986536c6a
Ezekiel A. Adetoro <adetoroe787@gmail.com>
2024-06-12 02:32:26 +0100

    meaning page and component

---
+++ src/compoenent/Meaning.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+export const Meaning = () => {
+  return (
+    <div>
+      <p className="mb-3 text-gray-400 dark:text-gray-300">Noun</p>
+      <p className="mb-3 text-gray-600 dark:text-gray-600 font-semibold">
+        The imparting or exchange of information or news
+      </p>
+      <p className="mb-3 text-gray-400 dark:text-gray-300">Example</p>
+      <p className="mb-3 text-gray-500 dark:text-gray-400 font-semibold">
+        &quot;at the moment I am in communication with London &ldquo;
+      </p>
+      <p className="mb-3 text-gray-400 dark:text-gray-300">Synonyms</p>
+      <ul className="flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
+        <li>
+          <a href="#" className="me-4 hover:underline md:me-6 ">
+            About
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
---
+++ src/pages/Definition.jsx
@@ -0,0 +1,13 @@
+import InputBox from "../compoenent/InputBox";
+import { Meaning } from "../compoenent/Meaning";
+
+const Home = () => {
+  return (
+    <>
+      <InputBox />
+      <Meaning />
+    </>
+  );
+};
+
+export default Home;
